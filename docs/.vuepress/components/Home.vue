688dd97e0fadb0c5cda4c073cc8ee68e6e868cdd
GimunLee <gimoon0226@naver.com>
2020-03-06 09:26:51 +0900

    Update HOME.vue

--- docs/.vuepress/components/Home.vue
+++ docs/.vuepress/components/Home.vue
@@ -5,11 +5,27 @@
         🏃‍♂️ Practice Makes Perfect.
       </h1>
     </div>
+
+    <div>
+      <h2>TEST</h2>
   </div>
+
+<div class="footer">
+  <hr/>
+  <p class="copyright">© 2020 GimunLee</p>    
+  <p class="copyright">This repository is licensed under the MIT license. See <a href="https://github.com/GimunLee/TIL/blob/master/LICENSE">LICENSE</a> for details.</p>
+</div>
 </template>
 
 <script>
 </script>
 
 <style>
+.footer{
+  text-align:center;
+}
+
+.copyright {
+  display:inline-block;
+}
 </style>
